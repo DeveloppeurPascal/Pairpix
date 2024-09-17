4c0a5a213a27299f53482574ee2646d4ad613195
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2024-09-17 20:14:19 +0200

    added header copyright comments (#41)

--- src/cBoutonPartage.pas
+++ src/cBoutonPartage.pas
@@ -1,3 +1,35 @@
+/// <summary>
+/// ***************************************************************************
+///
+/// Pairpix
+///
+/// Copyright 2022-2024 Patrick Prémartin under AGPL 3.0 license.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+/// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+/// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+/// DEALINGS IN THE SOFTWARE.
+///
+/// ***************************************************************************
+///
+/// Author(s) :
+/// Patrick PREMARTIN
+///
+/// Site :
+/// https://pairpix.gamolf.fr/
+///
+/// Project site :
+/// https://github.com/DeveloppeurPascal/Pairpix
+///
+/// ***************************************************************************
+/// File last update : 2024-09-17T19:00:44.663+02:00
+/// Signature : 3e3ac2fd240c20e6e8feee100954bdb766577f08
+/// ***************************************************************************
+/// </summary>
+
 unit cBoutonPartage;
 
 interface
--- src/uGameButton.pas
+++ src/uGameButton.pas
@@ -1,3 +1,35 @@
+/// <summary>
+/// ***************************************************************************
+///
+/// Pairpix
+///
+/// Copyright 2022-2024 Patrick Prémartin under AGPL 3.0 license.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+/// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+/// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+/// DEALINGS IN THE SOFTWARE.
+///
+/// ***************************************************************************
+///
+/// Author(s) :
+/// Patrick PREMARTIN
+///
+/// Site :
+/// https://pairpix.gamolf.fr/
+///
+/// Project site :
+/// https://github.com/DeveloppeurPascal/Pairpix
+///
+/// ***************************************************************************
+/// File last update : 2024-09-17T19:11:58.000+02:00
+/// Signature : c2b8754a70d18f0bd354f5d69f360b348a592ad1
+/// ***************************************************************************
+/// </summary>
+
 unit uGameButton;
 
 interface
